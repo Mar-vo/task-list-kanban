312ef3c32ec2ecdbc3a5aa1e834ddb0bdc3c3582
Mar-vo <46787993+Mar-vo@users.noreply.github.com>
2025-01-21 20:14:03 +0100

    Add global settings system and track installedAtVersion in data.json

--- src/entry.ts
+++ src/entry.ts
@@ -1,9 +1,26 @@
-import { MarkdownView, Plugin, TFile, WorkspaceLeaf } from "obsidian";
+import { App, MarkdownView, Plugin, TFile, WorkspaceLeaf } from "obsidian";
 import { KANBAN_VIEW_NAME, KanbanView } from "./ui/text_view";
 
+interface GlobalSettings {
+	installedAtVersion?: string;
+	defaultTaskPath?: string;
+}
+
+const defaultGlobalSettings: GlobalSettings = {};
+
 export default class Base extends Plugin {
+	globalSettings: GlobalSettings;
+
+	constructor(app: App, manifest: any) {
+		super(app, manifest);
+		this.globalSettings = defaultGlobalSettings;
+	}
+
 	async onload() {
 		this.registerView(KANBAN_VIEW_NAME, (leaf) => new KanbanView(leaf));
+
+		// Load and merge settings with defaults
+		await this.loadGlobalSettings();
 
 		this.switchToKanbanAfterLoad();
 
@@ -30,7 +47,9 @@ export default class Base extends Plugin {
 		});
 	}
 
-	onunload() {}
+	async onunload() {
+		await this.saveGlobalSettings();
+	}
 
 	private switchToKanbanAfterLoad() {
 		this.app.workspace.onLayoutReady(() => {
@@ -62,5 +81,48 @@ export default class Base extends Plugin {
 			type: KANBAN_VIEW_NAME,
 			state: leaf.view.getState(),
 		});
+	}
+
+	async loadGlobalSettings() {
+		// Retrieve any saved data
+		let savedData = await this.loadData();
+
+		if (!savedData || Object.keys(savedData).length === 0) {
+			await this.safeSetGlobalSettings(savedData || {});
+
+			if (!savedData || Object.keys(savedData).length === 0)
+				await this.saveGlobalSettings();
+		} else {
+			// just load the settings
+			await this.safeSetGlobalSettings(savedData);
+		}
+	}
+
+	private async safeSetGlobalSettings(settingsData: any) {
+		if (!settingsData || typeof settingsData !== "object") {
+			settingsData = {};
+		}
+
+		// in case saved data does not exist, the plugin was installed now or before 1.3, so add current version to settings, load them and save them
+		if (!settingsData.installedAtVersion) {
+			settingsData.installedAtVersion = this.manifest.version;
+
+			this.globalSettings = {
+				...defaultGlobalSettings,
+				...settingsData,
+			};
+			// Make sure to save the settings with the new installedAtVersion
+			await this.saveGlobalSettings();
+			return;
+		}
+
+		this.globalSettings = {
+			...defaultGlobalSettings,
+			...settingsData,
+		};
+	}
+
+	async saveGlobalSettings() {
+		await this.saveData(this.globalSettings);
 	}
 }
